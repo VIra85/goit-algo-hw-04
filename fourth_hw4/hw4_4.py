e5bb89da8796f01c6e09c98530223f7b9cd1dffe
VIra85 <voroninairyna@gmail.com>
2024-05-05 22:22:54 +0300

    Update hw4_4.py

--- fourth_hw4/hw4_4.py
+++ fourth_hw4/hw4_4.py
@@ -1,13 +1,30 @@
+def input_error(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except (KeyError, ValueError, IndexError) as e:
+            if isinstance(e, KeyError):
+                return "Enter user name"
+            elif isinstance(e, ValueError):
+                return "Enter the argument for the command"
+            elif isinstance(e, IndexError):
+                return "Invalid index"
+            
+    return wrapper
+
+@input_error
 def parse_input(user_input):
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
     return cmd, *args
 
+@input_error
 def add_contact(args, contacts):
     name, phone = args
     contacts[name] = phone
     return "Contact added."
 
+@input_error
 def change_contacts(username, new_phone, contacts):
     if username in contacts:
         contacts[username] = new_phone
@@ -15,13 +32,15 @@ def change_contacts(username, new_phone, contacts):
     else:
         return f"Error: {username} does not exist in the contacts." 
 
+@input_error
 def phone_user(args, contacts):
     name = args[0]
     if name in contacts:
         return f'{name}: {contacts[name]}'
     else:
         return "No user in contacts"
-
+    
+@input_error
 def show_all_contacts(contacts):
     if not contacts:
         return "No contacts available."
@@ -29,7 +48,8 @@ def show_all_contacts(contacts):
         for name, phone in contacts.items():
             print(f"{name}: {phone}")
         return "All contacts displayed."
-  
+    
+@input_error
 def main():
     contacts = {}
     print("Welcome to the assistant bot!")
@@ -51,7 +71,6 @@ def main():
             print(change_contacts(name, new_phone, contacts)) #3 Завдання
          
         elif command == "phone": 
-            # names = args
             print(phone_user(args, contacts))   #4
                 
         elif command == "all":
